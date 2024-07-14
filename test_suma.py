1373e8979c12d36dbbb218ef4976c6d357a6cfb6
Carlos Cote <unra321@gmail.com>
2024-07-14 09:40:28 +0200

    Git de correcion de codigo de workflow

--- test_suma.py
+++ test_suma.py
@@ -1,11 +1,10 @@
 import unittest
 from suma import sumar
-
-class test_sumar(unittest.TestCase):
-    def test_sumer(self):
-        self.assertEqual(sumar(3,2), 5)
-        self.assertEqual(sumar(-1,1), 0)
-        self.assertEqual(sumar(-1,-1), -2)
-
-if __name__ == "__main__":
+class TestSumar(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(sumar(3, 2), 5)
+        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, -1), -2)
+        
+if __name__ == '__main__':
     unittest.main()
